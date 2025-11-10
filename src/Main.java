b0cb19af78bf811fb0d26b7e184829694e999d70
Daniel <dsienes04@gmail.com>
2025-11-10 14:24:21 +0100

    pasajeros

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         //Completar Codigo
-        Balsa b1 = new Balsa(1 , 0.5);
-        Balsa b2 = new Balsa(2 , 1);
-        Balsa b3 = new Balsa(3 , 2);
-        Balsa b4 = new Balsa(4 , 4);
-        Balsa b5 = new Balsa(5 , 8);
+        List<Pasajero> pasajeros = new ArrayList<>();
+
+        Balsa bAcasta = new Balsa(1 , 0.5);
+        Balsa bBanff = new Balsa(2 , 1);
+        Balsa bCadiz = new Balsa(3 , 2);
+        Balsa bDeimos = new Balsa(4 , 4);
+        Balsa bExpedicion = new Balsa(5 , 8);
+
+        for (int i = 1 ; i <= 352; i++){
+            int prioridad = ((int) (Math.random()*(4-1)+1));
+            Pasajero pasajero = new Pasajero(i, prioridad);
+            pasajeros.add(pasajero);
+        }
+
+
+
 
     }
 }
