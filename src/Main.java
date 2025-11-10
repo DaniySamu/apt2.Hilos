09aa16d5ee9b4353ec07923889ef2c3b926b80f4
Daniel <dsienes04@gmail.com>
2025-11-10 13:32:53 +0100

    inicio

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         //Completar Codigo
+        Balsa b1 = new Balsa(1 , 1);
+        Balsa b2 = new Balsa(2 , 1);
+        Balsa b3 = new Balsa(3 , 2);
+        Balsa b4 = new Balsa(4 , 4);
+        Balsa b5 = new Balsa(5 , 8);
 
     }
 }
